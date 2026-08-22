f2f9e72fb3c89b30a9f9e01707e8f2368374a774
SuperdaveNLD <116336734+SuperdaveNLD@users.noreply.github.com>
2026-08-22 13:17:09 +0200

    Add data update coordinator

---
+++ custom_components/gw_energypilot/coordinator.py
@@ -0,0 +1,31 @@
+"""Data update coordinator for GW EnergyPilot."""
+
+from datetime import timedelta
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .client import GWEMSStatus, GWModbusClient, GWModbusError
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class GWEnergyPilotCoordinator(DataUpdateCoordinator[GWEMSStatus]):
+    """Coordinate polling of GoodWe EMS registers."""
+
+    def __init__(self, hass: HomeAssistant, client: GWModbusClient, scan_interval: int) -> None:
+        self.client = client
+        super().__init__(
+            hass,
+            logger=_LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=scan_interval),
+        )
+
+    async def _async_update_data(self) -> GWEMSStatus:
+        try:
+            return await self.client.async_read_status()
+        except GWModbusError as err:
+            raise UpdateFailed(str(err)) from err
